4ad5822ddb994999ee3d85788b35479b042aea88
Krint243 <krint243@gmail.com>
2021-06-19 19:20:57 +0300

    Lesson 1

---
+++ src/lesson1/MainClasss.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class MainClasss {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(5+7);
+        System.out.println("Test");
+    }
+}
